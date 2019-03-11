ea51f2d9db74e4306b39eed30f13fad08b0938b1
vmaron <vmaronmail@gmail.com>
2019-03-11 00:21:40 -0400

    Setup Android project

---
+++ app/src/main/java/com/vmaron/tvinsider/Activities/ShowDetailActivity.java
@@ -0,0 +1,16 @@
+package com.vmaron.tvinsider.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.vmaron.tvinsider.R;
+
+public class ShowDetailActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_detail);
+    }
+}
